02fafe55bf8ef938b15499a3802b2207baf9fe25
dhgo <castar2001kr@naver.com>
2023-03-02 04:02:45 +0900

    [new] first commit msg

---
+++ src/main/kotlin/com/app/onlinetodolist/OnlineTodoListApplication.kt
@@ -0,0 +1,11 @@
+package com.app.onlinetodolist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineTodoListApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnlineTodoListApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/onlinetodolist/OnlineTodoListApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.onlinetodolist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnlineTodoListApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
